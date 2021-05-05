76a072bf450f4527c9c5e9e6f049c232bb102910
Pratik <pratik@AlphaTPadLinux.local>
2021-05-06 03:21:18 +0530

    Swaybar modifications

--- sway/swaybar.sh
+++ sway/swaybar.sh
@@ -1,7 +1,10 @@
 # The Sway configuration file calls this script.
 
-battery_status=$(acpi | cut -d ',' -f2 | cut -d " " -f2)
+battery_percent=$(acpi | cut -d ',' -f2 | cut -d " " -f2)
+battery_charge_status=$(acpi | cut -d ',' -f1 | cut -d " " -f3)
+
 date_formatted=$(date "+%a %F %H:%M")
 
-echo "Battery: "$battery_status "|" $date_formatted
+
+echo "Battery: "$battery_percent $battery_charge_status "|" $date_formatted
 
