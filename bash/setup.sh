aa6001ae88cfd2dca0a3ff8ad3b824417a096785
Pratik Mullick <41203200+pratikmullick@users.noreply.github.com>
2025-09-20 20:29:09 +0530

    Restore deltas after reset

--- bash/setup.sh
+++ bash/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Create .bash_cred.
+# Create .bash_cred
 cred_file="$HOME/.bash_cred"
 read -p "Create Bash Credentials File? (Y/N): " cred_yn
 if [[ "$cred_yn" =~ ^([yY][eE][sS]|[yY])$ ]]
@@ -28,21 +28,37 @@ output_env_dir="$HOME/.config/environment.d"
 output_env_file="bashenv.conf"
 output_env_path="${output_env_dir}/${output_env_file}"
 regex="export"
-mkdir -p "$output_env_dir"
+# Check if output environment directory does not exist, creates it if true
+if ! test -d $output_env_dir; then
+    mkdir -p $output_env_dir
+fi
 
-while read -r line; do
-    parsed_line=$(awk '
-        NR == 1 && substr($0, 1, 1) == "#" { next }
-        {
-            gsub("'${regex}'", "");
-            if (substr($0, 1, 1) == " ") {
-                $0 = substr($0, 2);
-        }
-        print
-    }' <<< "$line")
-    echo "$parsed_line" >> "$output_env_path"
-done < "$input_env_file"
-echo ".bashenv -> ${output_env_path}."
+# Check if environment file exists, exits if true
+if test -f $output_env_path; then
+    echo "$output_env_path exists. Exiting.";
+    exit 0;
+else
+    # While loop for each environment variable
+    # Any line with comments in the bashenv are removed
+    # and the regular expression "export" is substituted using `gsub`
+    # including trailing space after it
+    while read -r line; do
+        parsed_line=$(awk '
+            NR == 1 && substr($0, 1, 1) == "#" { next }
+            {
+                gsub("'${regex}'", "");
+                if (substr($0, 1, 1) == " ") {
+                    $0 = substr($0, 2);
+                }
+                print
+            }' <<< "$line")
+        # Check parsed line for non-zero characters before redirecting output
+        if [ -n "$parsed_line" ]; then
+            echo "$parsed_line" >> "$output_env_path"
+        fi
+    done < "$input_env_file"
+    echo "./bashenv -> ${output_env_path}."
+fi
 
 # Clean exit
 exit 0
